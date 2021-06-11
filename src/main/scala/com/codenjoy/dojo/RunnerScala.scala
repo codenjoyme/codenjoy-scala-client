f1033baa0a856d5f0e46430cecdc981832b4f980
Roman Kulyniak <roman_kulyniak@epam.com>
2021-06-11 15:31:07 +0300

    [clients][scala] define separate RunnerScala

---
+++ src/main/scala/com/codenjoy/dojo/RunnerScala.scala
@@ -0,0 +1,19 @@
+package com.codenjoy.dojo
+
+import com.codenjoy.dojo.client.WebSocketRunner
+import com.codenjoy.dojo.client.runner.ReflectLoader.{loadScalaBoard, loadScalaSolver}
+
+object RunnerScala { // Select your game
+  var GAME = "bomberman"
+  // Paste here board page url from browser after registration,
+  // or put it as command line parameter.
+  var URL = "http://localhost:8080/codenjoy-contest/board/player/0?code=000000000000"
+
+  def main(args: Array[String]): Unit = {
+    if (args != null && args.length == 2) {
+      GAME = args(0)
+      URL = args(1)
+    }
+    WebSocketRunner.runClient(URL, loadScalaSolver(GAME), loadScalaBoard(GAME))
+  }
+}
