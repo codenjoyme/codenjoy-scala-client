b7d852e95861e5e65284acaf13a09ebe86d8ee27
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-17 00:06:36 +0200

    [clients][jvm] Made all jvm clients the same at the time of launching the bat file.

--- 2-build.bat
+++ 2-build.bat
@@ -5,7 +5,12 @@ call lib :color Building java client...
 echo on
 
 call lib :print_color %MVNW% -v
-call %MVNW% clean compile assembly:single -Pjar-with-dependencies -DskipTests=%SKIP_TESTS%
+
+rem install jar to maven local repo
+call %MVNW% clean install -DskipTests=%SKIP_TESTS%
+
+rem create executable jar
+call %MVNW% compile assembly:single -Pjar-with-dependencies -DskipTests=%SKIP_TESTS%
 echo F|xcopy /y %ROOT%\target\client-scala-exec.jar %ROOT%\app.jar
 
 call lib :ask
--- 3-run.bat
+++ 3-run.bat
@@ -4,9 +4,10 @@ echo off
 call lib :color Starting java client...
 echo on
 
+rem run jar
 call %JAVA% -Dfile.encoding=UTF-8 -jar %ROOT%/app.jar "%GAME_TO_RUN%" "%BOARD_URL%"
 
-rem another way to run
-rem call %MVNW% exec:java -Dfile.encoding=UTF-8 -D"exec.mainClass"="com.codenjoy.dojo.ScalaRunner" -D"exec.args"="%GAME_TO_RUN% %BOARD_URL%"
+rem build & run (without jar)
+rem call %MVNW% clean compile exec:java -Dfile.encoding=UTF-8 -D"exec.mainClass"="com.codenjoy.dojo.ScalaRunner" -D"exec.args"="%GAME_TO_RUN% %BOARD_URL%"
 
 call lib :ask
